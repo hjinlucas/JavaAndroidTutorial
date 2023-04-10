ced689be6d62932a9064ce7508972c1b3029420f
lucasjin <hjin.lucas@gmail.com>
2023-04-09 20:03:36 -0400

    Linear Layout Basics

---
+++ app/src/main/java/com/example/javaandroidtutorial/LinearLayoutActivity.java
@@ -0,0 +1,15 @@
+package com.example.javaandroidtutorial;
+
+import android.app.AppComponentFactory;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LinearLayoutActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_linear_layout);
+    }
+}
